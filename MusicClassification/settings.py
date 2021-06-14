59a5a2054bbdcbbbcc2db88c7ef98811dd463428
prasanna <67369708+prasannaMedia@users.noreply.github.com>
2021-06-14 14:07:39 +0530

    updates

--- MusicClassification/settings.py
+++ MusicClassification/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c2!j!65n$n8lbsp0vqk=(9#v9+irenmrvie(lqw5*)k&l!ku4#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['genreapp.herokuapp.com','localhost:8000']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
